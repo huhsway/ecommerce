1018c6cc699b4f256321b02d05451126eb51f362
seunghoe_heo <seunghoe_heo@tmax.co.kr>
2021-08-23 15:25:03 +0900

    Orders Microservice - 기능 구현 1

---
+++ order-service/src/main/java/com/example/orderservice/jpa/OrderEntity.java
@@ -0,0 +1,35 @@
+package com.example.orderservice.jpa;
+
+import lombok.Data;
+import org.hibernate.annotations.ColumnDefault;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Date;
+
+@Data
+@Entity
+@Table(name = "orders")
+public class OrderEntity implements Serializable { // 직렬화 하는 이유는 가지고 있는 객체를 네트워크전송이나 데이터베이스로 보관하기 위해서 마샬링 실시
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 120, unique = true)
+    private String productId;
+    @Column(nullable = false)
+    private Integer qty;
+    @Column(nullable = false)
+    private Integer unitPrice;
+    @Column(nullable = false)
+    private Integer totalPrice;
+
+    @Column(nullable = false)
+    private String userId;
+    @Column(nullable = false, unique = true)
+    private String orderId;
+
+    @Column(nullable = false, updatable = false, insertable = false)
+    @ColumnDefault(value = "CURRENT_TIMESTAMP")
+    private Date createdAt;
+}
